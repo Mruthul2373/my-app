3e582181802344fe3ddd484c12ac641915d5a276
EC2 Default User <ec2-user@ip-172-31-6-136.ap-south-1.compute.internal>
2026-06-11 06:06:52 +0000

    Add deployment workflow

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from Tomcat on EC2!</h1>");
+    }
+}
+
+
